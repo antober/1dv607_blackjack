bc1f44bde6c395d86d3c701e44e81349d6b45d53
anton oberg <ao222qg@student.lnu.se>
2018-10-27 21:57:13 +0200

    Winner and score is still bugged

--- BlackJack/model/rules/PlayerWinsOnEqual.cs
+++ BlackJack/model/rules/PlayerWinsOnEqual.cs
@@ -24,7 +24,7 @@ namespace BlackJack.model.rules
             }
             else
             {
-                return a_dealer.CalcScore() >= a_player.CalcScore();
+                return a_dealer.CalcScore() > a_player.CalcScore();
             }
         }
     }
